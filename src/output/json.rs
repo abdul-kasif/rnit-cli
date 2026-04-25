9dfbe85027a3fb50587291e008eec298bec13ede
abdul-kasif <abdulkasif.ra@gmail.com>
2026-04-25 15:10:34 +0530

    refactor(output): generalize JSON printer for any serializable type
    Refactor the JSON output module to support generic types implementing
    the Serialize trait.
    
    - Convert 'print_json' into a generic function 'print_json<T>'.
    - Remove the direct dependency on the 'fs::FileEntry' domain model.
    - Constraint the generic type T with 'serde::Serialize' to ensure
      compatibility.
    - Clean up internal variable naming for conciseness.
    
    This completes the generalization of the output layer, allowing both
    tabular and JSON formats to be used by any future domain modules.

--- src/output/json.rs
+++ src/output/json.rs
@@ -1,10 +1,9 @@
-use crate::domain::fs::FileEntry;
-use serde_json;
+use serde::Serialize;
 
-pub fn print_json(entries: &[FileEntry]) {
+pub fn print_json<T: Serialize>(entries: &[T]) {
     match serde_json::to_string_pretty(entries) {
-        Ok(json_string) => {
-            println!("{}", json_string)
+        Ok(json) => {
+            println!("{}", json)
         }
         Err(e) => eprintln!("Failed to serilalize to JSON: {}", e),
     }
